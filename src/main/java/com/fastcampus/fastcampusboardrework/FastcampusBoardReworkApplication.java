6cf5bf037982b90c31cc23fc37930502c5e96076
hingjae <ljh23270@naver.com>
2024-06-29 19:39:50 +0900

    first commit

---
+++ src/main/java/com/fastcampus/fastcampusboardrework/FastcampusBoardReworkApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.fastcampusboardrework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcampusBoardReworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastcampusBoardReworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fastcampus/fastcampusboardrework/FastcampusBoardReworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.fastcampusboardrework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastcampusBoardReworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
