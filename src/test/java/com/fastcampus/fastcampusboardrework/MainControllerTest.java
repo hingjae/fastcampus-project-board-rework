e4b818172e826dfbb14d22d3c4a1a4de28322c48
hingjae <ljh23270@naver.com>
2024-07-12 21:58:02 +0900

    코드 리팩토링

--- src/test/java/com/fastcampus/fastcampusboardrework/MainControllerTest.java
+++ src/test/java/com/fastcampus/fastcampusboardrework/MainControllerTest.java
@@ -1,13 +1,10 @@
 package com.fastcampus.fastcampusboardrework;
 
-import com.fastcampus.fastcampusboardrework.security.SecurityConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
